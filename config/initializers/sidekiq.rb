03583711b0d9eb2632f1b22c493aa8253710a34b
Lucas Haley <lucashaley@yahoo.com>
2019-11-05 21:34:39 +1300

    Re-activated sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
-# # Default is to retry 25 times with exponential backoff. That's too much.
-# Sidekiq.default_worker_options = { retry: 3 }
-#
-# if Rails.env.development?
-#   Sidekiq.default_worker_options[:backtrace] = true
-# end
+# Default is to retry 25 times with exponential backoff. That's too much.
+Sidekiq.default_worker_options = { retry: 3 }
+
+if Rails.env.development?
+  Sidekiq.default_worker_options[:backtrace] = true
+end
