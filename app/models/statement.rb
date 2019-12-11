2b49c39716bac01d803ed20740c8740a0af821f7
Lucas Haley <lucashaley@yahoo.com>
2019-12-11 23:12:17 +1300

    Missed part of updating vote_count to agree_count

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Statement < ApplicationRecord
-  has_closure_tree order: 'vote_count', numeric_order: true
+  has_closure_tree order: 'agree_count', numeric_order: true
   acts_as_taggable
   acts_as_voteable
 
